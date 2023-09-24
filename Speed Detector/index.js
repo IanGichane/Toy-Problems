1307790a3f400e7221a911e4cf5b17982396b9b1
Ian <gichaneian@gmail.com>
2023-09-24 21:07:03 +0300

    Added two files from the local repository

---
+++ Speed Detector/index.js
@@ -0,0 +1,48 @@
+/**
+ * 
+ takes the speed of a car as input e.g 80. 
+ If the speed is less than 70, it should print “Ok”. 
+ Otherwise, for every 5 km/s above the speed limit (70), 
+ it should give the driver one demerit point and print the total number of demerit points.
+
+   > For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+ */
+let speed= 80
+limit =70
+let demeritPoints=0
+console.log(speed)
+if(speed<=70){
+    console.log('Ok')
+}else{
+    //calculate speed above limit
+    let overSpeed=speed-limit
+    console.log(`you are ${overSpeed}km/h above speed limit`)
+    //calculate points given
+    let points = Math.floor(overSpeed/5)
+    console.log(points)
+    demeritPoints+=points
+    console.log(demeritPoints)
+    if(demeritPoints=24){
+        console.log('License suspended')
+        console.log(demeritPoints)
+    }
+
+}
+
+
+function speedCheck(){
+    let speed=Number(prompt('Enter your speed','speed in km/hr'))
+    console.log(speed)
+}
+
+
+//for every 5 km/s
+
+/**
+ * 0-4 0
+5 1
+5-9 1
+10 2
+11-14 2
+15 3
+ */
---
+++ student grades/index.js
@@ -0,0 +1,94 @@
+const studentGrade=Number(prompt(Enter number))
+}
+evaluateGrade()
+
+
+
+
+
+/**  if(studentGrade>79){
+            msg='You got an A'
+        }else if(studentGrade>=60 && studentGrade<79){
+            msg='You got an B'
+        }else if(studentGrade>=49 && studentGrade<=59){
+            msg='You got an C'
+        }else if(studentGrade>=40 && studentGrade<49){
+            msg='You got an D'
+        }else{
+            msg='You got an E'
+        } */
+/**
+ * 
+ * if(num>79){
+    console.log('A')
+}else if(num>=60 && num<79){
+    console.log('B')
+}else if(num>=49 && num<=59){
+    console.log('C')
+}else if(num>=40 && num<49){
+    console.log('D')
+}else{
+
+}
+ */
+
+/**
+ * 
+ * 
+ * switch(num){
+    case(>79):
+    msg='You got an A'
+    break;
+    case(>=60 &&<79):
+    msg='You got an B'
+    break;
+    case(>=49 && <=59 ):
+    msg='You got an C'
+    break;
+    case(>=40 && <49 ):
+    msg='You got an D'
+    break;
+    case(<40):
+    msg='You got an E'
+    break;
+}
+ */
+/**
+ * 
+let num =79
+
+switch(true){
+    case(num>79):
+        console.log('You got an A')
+    break;
+    case(num>=60 && num<79):
+    console.log('You got an A')
+    break;
+    case(num>=49 && num<=59 ):
+    console.log('You got an A')
+    break;
+    case(num>=40 && num<49 ):
+    console.log('You got an A')
+    break;
+    case(num<40):
+    console.log('You got an A')
+    break;
+}
+//Does not offer much of a difference to use the switch case alt
+//console.log(msg)
+
+/**
+ * 
+if(num>79){
+    console.log('A')
+}else if(num>=60 && num<79){
+    console.log('B')
+}else if(num>=49 && num<=59){
+    console.log('C')
+}else if(num>=40 && num<49){
+    console.log('D')
+}else{
+
+}
+ */
+ */
