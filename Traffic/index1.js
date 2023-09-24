17f5be093a4203600803555dbe047fa3cb9e64b9
Ian <gichaneian@gmail.com>
2023-09-24 21:46:35 +0300

    Completed this assignment

--- Traffic/index1.js
+++ Traffic/index1.js
@@ -1,13 +1,13 @@
 let countPoints=0
+//function to prompt user input
 function speedCheck(){
+    //variable declaration
     let speed=Number(prompt('Enter your speed','speed in km/hr'))
     let limit=70
-  
-    
     let msg='Your speed is '+ speed ;
     let msgOk;
     let tpoints;
-
+//Conditional to check the speed and calculate whether the driver is above or below the speed limit
     if(speed<=70){
         
         msgOk='Your speed is okay'
@@ -22,6 +22,7 @@ function speedCheck(){
         msgOk=`you are ${overSpeed} km/h above speed limit`
         dPoints=`you have received ${points} point(s)`
         countPoints = countPoints+points
+        //Condition to check state of the licence based of demerit points
         if(countPoints>=12){
             license='License suspended'
             
@@ -29,13 +30,11 @@ function speedCheck(){
             license='License not suspended'
         }
         tpoints=`you have a total of ${countPoints} demerit point(s)`
-      
     
     }
     //countPoints = countPoints+points
     //tpoints=`you have a total of ${countPoints} point(s)`
- 
-
+    //Accessing the DOM using the getelement by id to update the browser
 
     document.getElementById("message").innerHTML = msg;
     document.getElementById("okay").innerHTML = msgOk;
@@ -44,6 +43,5 @@ function speedCheck(){
     document.getElementById("license").innerHTML = license;
     
 }
-
-
+//Calling the function
 speedCheck()
