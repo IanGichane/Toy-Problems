fa6744f7a0e7279a92deb30d96274eaaef903bc0
Ian <gichaneian@gmail.com>
2023-09-24 21:11:36 +0300

    Added a traffic file from the local repository

---
+++ Traffic/index1.js
@@ -0,0 +1,37 @@
+let countPoints=0
+function speedCheck(){
+    let speed=Number(prompt('Enter your speed','speed in km/hr'))
+    let limit=70
+  
+    
+    let msg='Your speed is '+ speed ;
+    let msgOk;
+    let tpoints;
+
+    if(speed<=70){
+        
+        msgOk='Your speed is okay'
+        dPoints=`you have ${countPoints} points`
+        
+        
+
+        
+    }else{
+        let overSpeed=speed-limit
+        let points = Math.floor(overSpeed/5)
+        msgOk=`you are ${overSpeed} km/h above speed limit`
+        dPoints=`you have received ${points} point(s)`
+        countPoints = countPoints+points
+        tpoints=`you have a total of ${countPoints} point(s)`
+    }
+    //countPoints = countPoints+points
+    //tpoints=`you have a total of ${countPoints} point(s)`
+
+
+    document.getElementById("message").innerHTML = msg;
+    document.getElementById("okay").innerHTML = msgOk;
+    document.getElementById("driverPoints").innerHTML = dPoints;
+    document.getElementById("totalPoints").innerHTML = tpoints;
+    
+}
+speedCheck()
---
+++ Traffic/index2.js
@@ -0,0 +1,9 @@
+
+let countPoints = 0;
+let limit = 70;
+let speed = 80;
+let overSpeed = speed - limit;
+let points = Math.floor(overSpeed / 5);
+countPoints = countPoints + points;
+console.log(countPoints);
+
